6762345410a2f2cc1b08d3a09b1452842e8bac9e
JP Meijers <jp.meijers@canonical.com>
2025-03-05 14:26:56 +0200

    Add test for multiple components
    Add comment to describe smoke test

--- install_test.go
+++ install_test.go
@@ -7,9 +7,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+/*
+TestInstall runs `snapctl install` with the `--help` argument to do a smoke test of this function.
+It is only possible to do a real component installation for a snap that is in the snap store.
+*/
 func TestInstall(t *testing.T) {
 	t.Run("snapctl install --help valid-name", func(t *testing.T) {
 		err := snapctl.Install("mock-component-name").Help().Run()
+		require.NoError(t, err)
+	})
+
+	t.Run("snapctl install --help one two three", func(t *testing.T) {
+		err := snapctl.Install("one", "two", "three").Help().Run()
 		require.NoError(t, err)
 	})
 
