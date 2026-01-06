e4d03cfc2b892b1e775d824a7100ee66f01cb95b
JP Meijers <jp.meijers@canonical.com>
2026-01-06 16:27:21 +0200

    Set locale used by snapctl

--- run.go
+++ run.go
@@ -2,6 +2,7 @@ package snapctl
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 
@@ -14,7 +15,14 @@ func run(subcommand string, subargs ...string) (string, error) {
 
 	log.Debugf("Executing 'snapctl %s'\n", strings.Join(args, " "))
 
-	output, err := exec.Command("snapctl", args...).CombinedOutput()
+	cmd := exec.Command("snapctl", args...)
+	cmd.Env = os.Environ()
+
+	// Set locale to C to ensure output is in English
+	// This has no effect, as the host locale is being used by snapd, and this env var does not propagate across the REST API
+	cmd.Env = append(cmd.Env, "LANG=C.utf8")
+
+	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return "", fmt.Errorf("%w: %s", err, output)
 	}
